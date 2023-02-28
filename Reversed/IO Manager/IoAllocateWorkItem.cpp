d9ba69011b7f3ac0b6fbece1e4c34d02cc3022f4
Nana Bingies <nanabingies1@gmail.com>
2023-02-28 14:09:55 +0000

    Update IoAllocateWorkItem.cpp

--- Reversed/IO Manager/IoAllocateWorkItem.cpp
+++ Reversed/IO Manager/IoAllocateWorkItem.cpp
@@ -2,7 +2,7 @@ PIO_WORKITEM __fastcall IoAllocateWorkItem(__int64 DeviceObject)
 {
   IO_WORKITEM* IoWorkItem; // rax
 
-  IoWorkItem = IopVerifierExAllocatePool(0x200i64, 0x58i64);
+  IoWorkItem = IopVerifierExAllocatePool(NonPagedPoolNx, 0x58i64);
   if ( IoWorkItem )
   {
     *(_QWORD *)(IoWorkItem + 0x38) = 0i64;      // # struct _ETHREAD* WorkOnBehalfThread; 
