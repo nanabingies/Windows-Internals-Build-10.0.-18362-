83d81a8609c5732b644f1973207ceb4d2e3c22de
Emmanuel Sasu <nanabingies1@gmail.com>
2023-03-21 20:18:32 +0000

    Update MmIsAddressValid.cpp

--- Reversed/Memory Manager/MmIsAddressValid.cpp
+++ Reversed/Memory Manager/MmIsAddressValid.cpp
@@ -1,6 +1,7 @@
 char __fastcall MmIsAddressValidEx(unsigned __int64 VirtualAddress);
 
-__int64 MmIsAddressValid(PVOID BaseAddress)
+__int64 MmIsAddressValid(void* BaseAddress)
 {
-  return MmIsAddressValidEx(BaseAddress);
+  return MmIsAddressValidEx((unsigned __int64)BaseAddress);
 }
+
